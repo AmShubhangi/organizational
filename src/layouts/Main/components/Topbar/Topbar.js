a7b206344d29d1da36ebebd915f307c8b649374e
Jaydip Patel <jaydippatel83.pj@gmail.com>
2020-01-06 19:00:29 +0530

    css changes

--- src/layouts/Main/components/Topbar/Topbar.js
+++ src/layouts/Main/components/Topbar/Topbar.js
@@ -37,7 +37,7 @@ const Topbar = props => {
     >
       <Toolbar>
         <RouterLink to="/">
-          <h2 className="my-0 site-logo">WSOne OGs</h2>
+          <h2 className="my-0 site-logo text-white">WSOne OGs</h2>
         </RouterLink>
         <div className={classes.flexGrow} />
         <div>
